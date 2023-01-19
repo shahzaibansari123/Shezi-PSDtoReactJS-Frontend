c9d0f6e4f1075f126c6c727fafa9295f775a9504
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-01-19 20:29:27 +0500

    assets import

---
+++ src/constants/images.js
@@ -0,0 +1,31 @@
+import logo from "../assets/logo.png";
+import logo2x from "../assets/logo2x.png";
+
+import logosvg from "../assets/logosvg.png";
+import logosvg2x from "../assets/logosvg2x.png";
+
+import main from "../assets/main.png";
+import main2x from "../assets/main2x.png";
+
+import profile from "../assets/profile.png";
+import profile2x from "../assets/profile2x.png";
+import profile2 from "../assets/profile2.png";
+import profile22x from "../assets/profile22x.png";
+
+import productservices from "../assets/product&services.png";
+import productservices2x from "../assets/product&services2x.png";
+import productservices2 from "../assets/product&services2.png";
+import productservices22x from "../assets/product&services22x.png";
+
+import project1 from "../assets/project1.png";
+import project12x from "../assets/project12x.png";
+import project2 from "../assets/project2.png";
+import project22x from "../assets/project22x.png";
+import project3 from "../assets/project3.png";
+import project32x from "../assets/project32x.png";
+
+import equipmentfacilities2x from "../assets/equipment&facilities2x.png";
+import equipmentfacilities from "../assets/equipment&facilities.png";
+
+import contact from "../assets/contact.png";
+import contact2x from "../assets/contact2x.png";
